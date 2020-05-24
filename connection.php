663db076516565fd823c56add1fc0719b56c327d
Joeal11 <57830635+Joeal11@users.noreply.github.com>
2020-05-24 15:58:32 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,12 +1,10 @@
 <?php 
  
- $host = 'rssjoeal.database.windows.net';
- $login_bazy_danych = 'joeal'; 
+ $host = 'joealdatabase.mysql.database.azure.com';
+ $login_bazy_danych = 'joeal@joealdatabase'; 
  $haslo_bazy_danych = 'Alabama11';
- $nazwa_bazy_danych = 'RSS_DB_Mail';
+ $nazwa_bazy_danych = 'RSS_DB';
 
- $table_mail = '';
- $table_url = '':
  
  $connection = mysqli_connect($host, $login_bazy_danych,$haslo_bazy_danych, $nazwa_bazy_danych);
  
